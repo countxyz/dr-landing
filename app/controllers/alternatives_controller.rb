f55733bc8d84506c61b8c1cffab5a38f345b6430
count_xyz <skeptoid@gmail.com>
2015-05-23 14:25:01 -0400

    Implement flexbox kit in header.

---
+++ app/controllers/alternatives_controller.rb
@@ -0,0 +1,4 @@
+class AlternativesController < ApplicationController
+
+  def landing; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'alternative', to: 'alternatives#landing'
+
   root 'home#landing'
 end
